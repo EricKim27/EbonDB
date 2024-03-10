c5eea3c74a1787c1e036357ade6bf863658c52f7
EricKim27 <seohyun.kim@outlook.kr>
2024-03-10 22:14:07 +0900

    changed request.commandinterpret() so that it could work in server-client model.

--- request.py
+++ request.py
@@ -9,6 +9,7 @@ class Request:
         self.request = req
         self.user = user
         self.flag = flags
+        self.out = []
     def stripcmd(self):
         return self.request.split(' ')
     def commandinterpret(self):
@@ -24,11 +25,11 @@ class Request:
                             primelist.append([line.strip("\n")])
                             header = ['Databases']
                     table = tabulate(primelist, headers=header, tablefmt="fancy_grid")
-                    print(table)
+                    self.out.append(table)
                     result = 0
                 elif strip[1] == "tables":
                     if self.flag == ' ':
-                        print("database not selected")
+                        self.out.append("database not selected")
                         result = 1
                     else:
                         primelist = []
@@ -40,44 +41,39 @@ class Request:
                             for line in data:
                                 primelist.append([line.strip('\n')])
                         table = tabulate(primelist, headers=header, tablefmt="fancy_grid")
-                        print(table)
+                        self.out.append(table)
                         result = 0
                 else:
-                    print("syntax error")
+                    self.out.append("Syntax error")
                     result = 2
             else:
-                print("syntax error")
+                self.out.append("Syntax error")
                 result = 1
         elif strip[0] == "mkdb":
             database = db.DB(strip[1])
             database.mkdb()
             result = 0
         elif strip[0] == "rmdb":
-            doublecheck = input("Are you sure you want to delete the Database: {0}?(y/n)".format(strip[1]))
-            if doublecheck == "y":
-                db_to_remove = db.DB(strip[1])
-                db_to_remove.rmdb()
-                print("Database removed.")
-                result = 0
-            elif doublecheck == "n":
-                print("aborted")
-                result = 0
-            else:
-                print("wrong syntax!")
-                result = 1
+            # temporarily deprecated because I can't think of a way to make it work.
+            # doublecheck = input("Are you sure you want to delete the Database: {0}?(y/n)".format(strip[1]))
+            db_to_remove = db.DB(strip[1])
+            db_to_remove.rmdb()
+            self.out.append("Database removed.")
+            result = 0
         elif strip[0] == "rmtable":
             if self.flag == ' ':
-                print("database not selected")
+                self.out.append("database not selected")
                 result = 1
             else:
-                doublecheck = input("Are you sure you want to delete table: {0}?(y/n)".format(strip[1]))
-                if doublecheck == "y":
-                    table_to_remove = db.Table(strip[1], self.flag)
-                    table_to_remove.rmtable()
-                    result = 0
+                # temporarily deprecated because I can't think of a way to make it work.
+                # doublecheck = input("Are you sure you want to delete table: {0}?(y/n)".format(strip[1]))
+                table_to_remove = db.Table(strip[1], self.flag)
+                table_to_remove.rmtable()
+                self.out.append("Table removed.")
+                result = 0
         elif strip[0] == "mkcolumn":
             if self.flag == ' ':
-                print('database not selected')
+                self.out.append('database not selected')
                 result = 1
             else:
                 classdata = strip[2].strip('{').strip('}').split(',')
@@ -89,14 +85,14 @@ class Request:
         elif strip[0] == "usedb":
             if os.path.isdir("{0}/db/{1}".format(rootpath, strip[1])):
                 self.flag = str(strip[1])
-                print("Database changed to {}".format(strip[1]))
+                self.out.append("Database changed to {}".format(strip[1]))
                 result = 0
             else:
-                print("Database not found")
+                self.out.append("Database not found")
                 result = 1
         elif strip[0] == "mktable":
             if self.flag == ' ':
-                print("database not selected.")
+                self.out.append("database not selected.")
                 result = 1
             else:
                 table = db.Table(strip[1], self.flag)
@@ -105,7 +101,7 @@ class Request:
         elif strip[0] == "insert":
             if len(strip) == 4:
                 if self.flag == ' ':
-                    print("database not selected.")
+                    self.out.append("database not selected.")
                     result = 1
                 else:
                     columninfo = strip[2].strip('{').strip('}').split(',')
@@ -113,19 +109,19 @@ class Request:
                     for i in range(len(columninfo)):
                         type = db.checkclass(self.flag, strip[1], columninfo[i])
                         if type == "error":
-                            print('column {} was not found'.format(columninfo[i]))
+                            self.out.append('column {} was not found'.format(columninfo[i]))
                             result = 1
                         else:
                             data_for_write = db.Data(type, columninfo[i], datainfo[i])
                             data_for_write.writedata(self.flag, strip[1])
                             result = 0
             else:
-                print("Syntax error")
+                self.out.append("Syntax error")
                 result = 2
         #the get function is used for getting data of columns in the table.
         elif strip[0] == "get":
             if self.flag == ' ':
-                print("database not selected.")
+                self.out.append("database not selected.")
                 result = 1
             else:
                 if strip[1] == "*":
@@ -160,11 +156,11 @@ class Request:
                                     primelist[i].append(str(value[2].strip("\n")))
                 primelist_aligned = list(zip(*primelist))
                 table = tabulate(primelist_aligned, headers=columndata, tablefmt="fancy_grid")
-                print(table)
+                self.out.append(table)
                 result = 0
         else:
-            print("Command not found")
+            self.out.append("Command not found")
             result = 1
         if result == 200:
-            print("error in code. contact developer at github.")
-        return result
+            self.out.append("error in code. contact developer at github.")
+        return self.out, str(self.flag), result
