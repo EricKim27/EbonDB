99775cf1ea3ffd678f355c3c327180c8bfb997aa
Eric Kim <seohyun.kim@outlook.kr>
2024-03-20 17:53:55 +0900

    fixed dependency issues between code.

--- db.py
+++ db.py
@@ -211,3 +211,32 @@ class Server:
             else:
                 print(Exception)
                 break
+class Permission:
+    def add_new_user_permission(user, db):
+        filename = "{0}/admin/Permission.data".format(rootpath)
+        data = "{0}:{1}\n".format(user, db)
+        with open(filename, 'a+') as f:
+            f.write(data)
+    def add_existing_user_permission(user, db):
+        filename = "{0}/admin/Permission.data".format(rootpath)
+        with open(filename, "r") as f:
+            data = f.readlines()
+            for line in data:
+                if line.split(":")[0] == user:
+                    changed_line = line.split(":")[0] + ":" + line.split(":")[1].strip("\n") + f",{db}\n"
+        with open(filename, "w") as f:
+            for line in data:
+                if line.split(":")[0] == user:
+                    f.write(changed_line)
+                else:
+                    f.write(line)
+    def check_permission(user, db):
+        filename = f"{rootpath}/admin/Permission.data"
+        with open(filename, "r") as f:
+            data = f.readlines()
+            for line in data:
+                if line.split(":")[0] == user:
+                    if db in line.split(":")[1].strip("\n").split(","):
+                        return True
+                    else:
+                        return False
